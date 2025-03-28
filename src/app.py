daa4da1a0681837bb4dc8b8a33d6d37ca41b6a2f
Umakaran Nemallan <unemallan@gmail.com>
2025-03-28 12:19:39 -0400

    Added Logic to fetch weather from OM

--- src/app.py
+++ src/app.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 
+import requests
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Weather.sqlite3'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+'''
+Define the database model
+that is used to store 
+the temperature.
+'''
+
+class Weather(db.Model):
+    datetime = db.Column(db.DateTime, primary_key=True, default=datetime.utcnow())
+    temperature = db.Column(db.Integer, nullable=False)
+    
+'''
+Helper function to get temperature
+using API
+'''
+
+def get_temperature():
+    response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+    if (
+      response.status_code != 204 and
+      response.headers["content-type"].strip().startswith("application/json")
+    ):
+      try:
+        #print (response.json())
+        return response.json()["current"]["temperature_2m"]
+      except ValueError:
+        return None
+
 
 @app.route("/")
 def index():
+   db.create_all()
+   current_temperature = get_temperature()
+   new_entry = Weather(temperature=current_temperature)
+   db.session.add(new_entry)
+   db.session.commit()
    return render_template('index.html')
-
+  
 if __name__ == '__main__':
-   app.run(debug=False)
+   app.run(debug=True)
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    current_temperature = Weather.query.first()
+    return "You entered: " + input_text + "current temperature is:" + str(current_temperature.temperature)
+
